b4ff40bfd1cadb53dd19cc10b2b3bc57792ca01c
Karlos <loboscarlos1704@gmail.com>
2026-08-31 15:38:08 -0600

    tarjetas de productos

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,15 +2,19 @@ import type React from "react";
 import { Drawer } from "../components/Drawer";
 import { useState } from "react";
 import { Menu } from "lucide-react";
+import { useAuth } from "../context/useAuth";
+import { ProductCard } from "../components/ProductCard";
+import { products } from "../data/products";
 export const DashboardPage: React.FC = () => {
+    const {logout, user}=useAuth()
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
     return (
-        <div className="min-h-screen bg-slate-50">
+        <div className="flex min-h-screen bg-slate-50">
             <Drawer
                 isOpen={isDrawerOpen}
                 onClose={() => setIsDrawerOpen(false)}
             />
-            <main className="md:ml-0">
+            <main className="min-w-0 flex-1">
                 <header className="flex h-16 items-center justify-between border-b border-slate-200 bg-white px-5">
                     <button
                         type="button"
@@ -20,7 +24,35 @@ export const DashboardPage: React.FC = () => {
                     >
                         <Menu size={20} />
                     </button>
+                    <h1 className="text-xl font-semibold text-slate-800">
+                        Catalogo de productos
+                    </h1>
+                    <button
+                    type="button"
+                    onClick={logout}
+                    className="rounded-lg bg-red-400 px-4 py-2 text-sm font-semibold text-white hover:bg-red-700"
+                    >
+                        Cerrar sesión
+                    </button>
                 </header>
+                <section
+                className="p-6"
+                >
+                    <div className="mb-8">
+                        <h2 className="text-3xl font-normal font-black text-slate-900">
+                            Comida lista para entregar a la puerta de tu casa
+                        </h2>
+                        <p className="mt-2 text-sm text-slate-600">
+                            Descrubre los mejores productos para ti, { " "}
+                            {user?.name}
+                        </p>
+                    </div>
+                    <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-4">
+                        {products.map((product)=>(
+                            <ProductCard key={product.id} product={product}/>
+                        ))}
+                    </div>
+                </section>
             </main>
         </div>
     )
